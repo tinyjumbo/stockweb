f11855e13a324358528a322779b6f75cfb886de5
Jelly-Yu <jelly.yu1992@gmail.com>
2016-02-10 16:19:42 -0500

    created layout files

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
 @app.route("/")
-def hello():
-    return "Go, Tiny Jumbo!"
+def index():
+    return render_template('index.html')
+
+
 if __name__ == "__main__":
     app.run()

c4a1ef5999cdd47952c0bfc5e90687a80c2898d4
hyperchi <hyperbolechi@gmail.com>
2016-02-15 20:42:46 -0500

    now it is clean

--- FlaskApp/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, render_template, redirect, url_for, request
-app = Flask(__name__)
-from app import views
--- FlaskApp/views.py
+++
@@ -1,5 +0,0 @@
-from app import app
-# all routes should go here
-@app.route("/")
-def index():
-    return render_template('index.html')

275c27a68ac58e3ce40f06fbc21faad19f70147e
zhihui <zhihui.qiu.tufts@gmail.com>
2016-02-17 23:43:40 -0500

    MongoDB setted up

--- FlaskApp/app/views.py
+++ FlaskApp/app/views.py
@@ -1,4 +1,8 @@
-from FlaskApp.app import app
+#test mode
+from app import app
+#server mode
+#from FlaskApp.app import app
+
 from flask import render_template, redirect, url_for, request
 # this file should works as part of controllers for routes
 # if we have more actions later we can have a folder for controllers later but now lets keep it here.

c85585d69395e150ff6782374b80955b821610c7
zhihui <zhihui.qiu.tufts@gmail.com>
2016-03-05 11:12:38 -0500

    milestone DBschema100%

--- FlaskApp/app/DBdriver.py
+++ FlaskApp/app/DBdriver.py
@@ -4,6 +4,7 @@ import pymongo
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 import config
+from app.models.tcount import tcount
 
 
 
@@ -19,26 +20,25 @@ class DBdriver():
 
 	def read_barchar(self):
 
-		self.tweet_collection = self.db.tcount
+		#future will not be hardcode time
+		start = datetime(2016, 2, 18, 20, 01, 01)
+		end = datetime(2016, 2, 19,20 , 01, 04)
+
+		tweet_collection = self.db.tcount
 		 #read from DB and store it
 		print "start to get collections"
-		self.data_set = []
-		self.count_set = []
+		count_set = []
 		print "create an empty set"
 
 
-		self.now = datetime.now()
-		self.start = datetime(2016, 2, 18, 20, 01, 01)
-		self.end = datetime(2016, 2, 19,20 , 01, 04)
-		self.db_read = self.tweet_collection.find({'date': {'$gte': self.start, '$lt': self.end}}).sort([("company",pymongo.ASCENDING)])
-		self.db_read_num = self.tweet_collection.find({'date': {'$gte': self.start, '$lt': self.end}}).count()
+		
+		db_read = tweet_collection.find({'date': {'$gte': start, '$lt': end}}).sort([("company",pymongo.ASCENDING)])
+		#db_read_num = tweet_collection.find({'date': {'$gte': start, '$lt': end}}).count()
 		#db_read = tweet_collection.find_one()
-		print self.db_read
-		print self.db_read_num
 		print "succeed to read"
-		for i in self.db_read:
-			self.count_set.append(i["count"])
-
-		return self.count_set
+		for record in db_read:
+			tcount_obejct=tcount(company=record["company"],count=record["count"],date=record["date"])
+			count_set.append(tcount_obejct.count)
+		return count_set
 
 

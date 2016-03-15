0d65d51b0a8e0260f2f3d48cb2e5a1446c096d94
Jelly-Yu <jelly.yu1992@gmail.com>
2016-03-14 23:30:09 -0400

    recommit after git corrupt loose object

--- FlaskApp/app/static/js/script.js
+++ FlaskApp/app/static/js/script.js
@@ -1,71 +1,126 @@
 /* this is a js file for index page */
-
-function createChart(seriesOptions, divId) 
-{
-    $(divId).highcharts('StockChart', {
-
-        rangeSelector: {
-            selected: 4
+var slideIndex = 1;
+function bar_chart(){
+	    $('#container1').highcharts({
+        data: {
+            table: 'datatable'
         },
-
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Tweets count'
+        },
         yAxis: {
-            labels: {
-                formatter: function () {
-                    return (this.value > 0 ? ' + ' : '') + this.value + '%';
-                }
-            },
-            plotLines: [{
-                value: 0,
-                width: 2,
-                color: 'silver'
-            }]
-        },
-
-        plotOptions: {
-            series: {
-                compare: 'percent'
+            allowDecimals: false,
+            title: {
+                text: 'Units'
             }
         },
-
         tooltip: {
-            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-            valueDecimals: 2
-        },
-
-        series: seriesOptions
-    });
-}
-
-// combine charts together
-function combineCharts(divId, stockSymbols, seriesOptions, seriesCounter) 
-{
-    /**
-     * Create the chart when all data is loaded
-     * @returns {undefined}
-     */
-   $.each(stockSymbols, function (i, name) 
-   {
-        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?', function (data) 
-        {
-
-            seriesOptions[i] = {
-                name: name,
-                data: data
-            };
-            // As we're loading the data asynchronously, we don't know what order it will arrive. So
-            // we keep a counter and create the chart when all the data is loaded.
-            seriesCounter += 1;
-            if (seriesCounter === stockSymbols.length) 
-            {
-                createChart(seriesOptions, divId);
+            formatter: function () {
+                return '<b>' + this.series.name + '</b><br/>' +
+                    this.point.y + ' ' + this.point.name.toLowerCase();
             }
-        });
+        }
     });
+	
+}
+function tweet_card(){
+	
+	
 }
 
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("tweet-card");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+function line_chart(){
+	$.ajax({
+        url: "https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?",
+        dataType: 'json',
+        error: function(xhr, textStatus, errorThrown){
+            console.log("sth bad happened");
+
+        },
+        success: function(data){
+            console.log(data);
+            console.log("I want to die");
+            $('#container0').highcharts('StockChart', {
+
+
+                rangeSelector : {
+                    selected : 1
+                },
+
+                title : {
+                    text : 'AAPL Stock Price'
+                },
+
+                series : [{
+                    name : 'AAPL',
+                    data : data,
+                    tooltip: {
+                        valueDecimals: 2
+                    }
+                }]
+            });
+        }
+    });
+}
 // main function to call charts
 $(document).ready(function () {
-    var stockSymbolsG = ['MSFT', 'AAPL', 'GOOG'];
-    combineCharts("#container", stockSymbolsG, [], 0);
-    // add more charts here
+	line_chart();
+	bar_chart();
+    
+    showDivs(slideIndex);
+    $('.tabs nav a').on('click', function() {
+        show_content($(this).index());
+    });
+
+    show_content(0);
+
+    $('#company-select').searchableOptionList({
+        maxHeight: '100px'
+    });
+
+    $( "#from" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3,
+        onClose: function( selectedDate ) {
+            $( "#to" ).datepicker( "option", "minDate", selectedDate );
+        }
+    });
+    $( "#to" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3,
+        onClose: function( selectedDate ) {
+            $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+        }
+    });
 });
+function show_content(index) {
+    // Make the content visible
+    $(".tabs .content").hide();
+    $("#container"+index).show();
+
+    // Set the tab to selected
+    $('.tabs nav a.selected').removeClass('selected');
+    $('.tabs nav a:nth-of-type(' + (index + 1) + ')').addClass('selected');
+}

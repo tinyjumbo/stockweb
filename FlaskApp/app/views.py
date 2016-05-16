752cc8f9e037805cec5692d3d40e6b65f838ef44
qiuzhihui <zhihui.qiu.tufts@gmail.com>
2016-05-15 22:33:20 -0400

    Modified wsgi

--- FlaskApp/app/views.py
+++ FlaskApp/app/views.py
@@ -1,10 +1,5 @@
 #test mode
 from app import app
-#server mode
-#from FlaskApp.app import app
-# dude these schema things should not be in the view.. this basically means
-# everytime when you refresh the page you have to reconnect the server??
-# I believe this should be in init file and the you shoudl find a way to maintain through model.
 from datetime import datetime, timedelta
 from flask import render_template, redirect, url_for, request
 import pymongo
@@ -13,24 +8,6 @@ from pymongo.errors import ConnectionFailure
 import logging
 from logging import Formatter, FileHandler
 from DBdriver import DBdriver
-
-
-#Setup the logger
-LOGGER = logging.getLogger('streamer_logger')
-file_handler = FileHandler('streamer.log')
-handler = logging.StreamHandler()
-file_handler.setFormatter(Formatter(
-        '%(thread)d %(asctime)s %(levelname)s: %(message)s '
-        '[in %(pathname)s:%(lineno)d]'
-))
-handler.setFormatter(Formatter(
-        '%(thread)d %(asctime)s %(levelname)s: %(message)s '
-        '[in %(pathname)s:%(lineno)d]'
-))
-LOGGER.addHandler(file_handler)
-LOGGER.addHandler(handler)
-LOGGER.setLevel(logging.DEBUG)
-
 
 
 
--- flaskapp.wsgi
+++ flaskapp.wsgi
@@ -2,7 +2,10 @@
 import sys
 import logging
 logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/var/www/FlaskApp/")
+sys.path.insert(0,"/var/www/FlaskApp/FlaskApp")
 
-from FlaskApp import app as application
+
+
+from app import app as application
 application.secret_key = 'tinyjumbo'
+
